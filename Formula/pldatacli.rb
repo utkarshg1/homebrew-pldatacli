8f02e350cc5fa7b2ae17d39e83226fbeb17ff456
utkarshg1 <gaikwadujg@gmail.com>
2026-03-14 11:17:36 +0000

    updated formula using uv

--- Formula/pldatacli.rb
+++ Formula/pldatacli.rb
@@ -5,10 +5,10 @@ class Pldatacli < Formula
   sha256 "fbacf7e2046eca81e8e62b0194ff74a3f1441892667f4b9c72767722cad1c408"
   license "MIT"
 
-  depends_on "python@3.12"
+  depends_on "uv" => :build
 
   def install
-    system "python3.12", "-m", "pip", "install", *std_pip_args, "pldatacli"
+    system "uv", "tool", "install", "pldatacli", "--tool-bin-dir", bin
   end
 
   test do
